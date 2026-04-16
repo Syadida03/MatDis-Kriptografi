26010dee2dd16aa5a504137576df22050253c101
Samsul <jstzzsamsull@gmail.com>
2026-04-16 22:24:30 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
-function handleProcess(isEncrypt) {
+function handleProcess(encrypt) {
     const text = document.getElementById('inputText').value;
     let key = parseInt(document.getElementById('keyInput').value) || 0;
     const resultDisplay = document.getElementById('resultText');
 
-    let output = "";
+    let output = "" ;
 
     for (let i = 0; i < text.length; i++) {
         // Ambil kode ASCII asli (sama dengan ord(p) di Pascal)
         const code = text.charCodeAt(i);
         
         let newCode;
-        if (isEncrypt) {
+        if (encrypt) {
             // RUMUS ENKRIPSI DI GAMBAR: (ord(p) + k) mod 256
             newCode = (code + key) % 256;
+                    // 32      3   
         } else {
             // RUMUS DEKRIPSI DI GAMBAR: (ord(p) - k) mod 256
             // Ditambah 256 sebelum mod agar tidak menghasilkan angka negatif
@@ -25,3 +26,12 @@ function handleProcess(isEncrypt) {
 
     resultDisplay.innerText = output;
 }
+
+
+
+// a b c d e f g h i j k       !  "   #  
+// 0 1 2 3                32   33 34  35
+
+
+
+
